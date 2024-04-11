896b00e46e4ca80f27605e64351825e3e1bf1853
veslorand <veslorand@gmail.com>
2024-04-11 22:29:50 +0200

    Add new SVG files for happy and sad moods, and implement MoodNft contract

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -2,31 +2,53 @@
 
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
-
-contract MoodNft is ERC721{
+contract MoodNft is ERC721 {
     uint256 private s_tokenCounter;
-    string private s_sadSvg;
-    string private s_happySvg;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
 
+    enum Mood {
+        HAPPY,
+        SAD
+    }
 
-    constructor(string memory sadSvg, string memory happySvg) ERC721("Mood NFT", "MN") {
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
+    ) ERC721("Mood NFT", "MN") {
         s_tokenCounter = 0;
-        s_sadSvg = sadSvg;
-        s_happySvg = happySvg;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
     }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        if (tokenId % 2 == 0) {
-            return s_sadSvg;
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageURI;
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            imageURI = s_happySvgImageUri;
         } else {
-            return s_happySvg;
+            imageURI = s_sadSvgImageUri;
         }
+
+        string memory tokenMetaData = string.concat(
+            '{"name":"',
+            name(), // You can add whatever name here
+            '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+            '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+            imageURI,
+            '"}'
+        );
     }
 }
